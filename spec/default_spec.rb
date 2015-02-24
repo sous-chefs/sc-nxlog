aa3b047092148520348012b86effb07b68f3f3dd
Simon Detheridge <simon@widgit.com>
2015-02-24 17:39:38 +0000

    Add unit tests for main config file

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -8,4 +8,14 @@ describe 'nxlog-ce::default' do
     expect(chef_run).to install_dpkg_package('nxlog-ce')
     expect(chef_run).to enable_service('nxlog')
   end
+
+  it 'creates a config file' do
+    expect(chef_run).to render_file('/etc/nxlog/nxlog.conf').with_content(/User *nxlog/)
+    expect(chef_run).not_to render_file('/etc/nxlog/nxlog.conf').with_content(/SpoolDir/)
+    expect(chef_run.service('nxlog')).to subscribe_to('template[/etc/nxlog/nxlog.conf]')
+  end
+
+  it 'creates a config directory' do
+    expect(chef_run).to create_directory('/etc/nxlog/nxlog.conf.d')
+  end
 end
