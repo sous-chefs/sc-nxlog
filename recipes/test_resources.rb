8da26b226bc5072f54fac2c963d7422cff90bcb6
Simon Detheridge <simon@widgit.com>
2015-03-03 14:33:51 +0000

    Add tests for all remaining output modules

--- recipes/test_resources.rb
+++ recipes/test_resources.rb
@@ -26,3 +26,63 @@ end
 nxlog_ce_destination 'test_om_blocker' do
   output_module 'om_blocker'
 end
+
+nxlog_ce_destination 'test_om_dbi' do
+  output_module 'om_dbi'
+  driver 'mysql'
+  sql 'INSERT INTO log VALUES ($SyslogFacility, $SyslogSeverity, $Message)'
+  options ['host 127.0.0.1', 'username foo', 'password bar', 'dbname nxlog']
+end
+
+nxlog_ce_destination 'test_om_exec' do
+  output_module 'om_exec'
+  command '/usr/bin/foo'
+  args %w(bar baz)
+end
+
+nxlog_ce_destination 'test_om_https' do
+  output_module 'om_http'
+  url 'https://example.org/foo'
+  https_cert_file '%CERTDIR%/client-cert.pem'
+  https_cert_key_file '%CERTDIR%/client-key.pem'
+  https_ca_file '%CERTDIR%/ca.pem'
+  https_allow_untrusted false
+end
+
+nxlog_ce_destination 'test_om_http' do
+  output_module 'om_http'
+  url 'http://example.org/bar'
+end
+
+nxlog_ce_destination 'test_om_null' do
+  output_module 'om_null'
+end
+
+nxlog_ce_destination 'test_om_ssl' do
+  output_module 'om_ssl'
+  port 1234
+  host 'logs.example.org'
+  cert_file '%CERTDIR%/client-cert.pem'
+  cert_key_file '%CERTDIR%/client-key.pem'
+  ca_file '%CERTDIR%/ca.pem'
+  allow_untrusted false
+  output_type 'Binary'
+end
+
+nxlog_ce_destination 'test_om_tcp' do
+  output_module 'om_ssl'
+  port 1234
+  host 'logs.example.org'
+end
+
+nxlog_ce_destination 'test_om_udp' do
+  output_module 'om_tcp'
+  port 1234
+  host 'logs.example.org'
+end
+
+nxlog_ce_destination 'test_om_uds' do
+  output_module 'om_uds'
+  exec 'parse_syslog_bsd(); to_syslog_bsd();'
+  uds '/dev/log'
+end
