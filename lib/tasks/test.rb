ea071fc0936e2812992582976e7d4a51a6fe07f1
Simon Detheridge <simon@widgit.com>
2015-04-22 10:07:29 +0100

    Fix rubocop errors in rake tasks

--- lib/tasks/test.rb
+++ lib/tasks/test.rb
@@ -4,23 +4,25 @@ task test: 'test:quick'
 namespace :test do
   desc 'Run all tests except kitchen (which takes a long time)'
   task :quick do
-    raise "tests failed" unless system 'bundle exec strainer test -e kitchen'
+    fail 'tests failed' unless system 'bundle exec strainer test -e kitchen'
   end
 
   desc 'Run all tests, including kitchen'
   task :all do
-    raise "tests failed" unless system 'bundle exec strainer test'
+    fail 'tests failed' unless system 'bundle exec strainer test'
   end
 
   namespace :debug do
     desc 'Run all tests except kitchen, with debugging enabled'
     task :quick do
-      raise "tests failed" unless system 'bundle exec strainer test -e kitchen --log-level=debug'
+      fail 'tests failed' \
+        unless system 'bundle exec strainer test -e kitchen --log-level=debug'
     end
 
     desc 'Run all tests, including kitchen, with debugging enabled'
     task :all do
-      raise "tests failed" unless system 'bundle exec strainer test --log-level=debug'
+      fail 'tests failed' \
+        unless system 'bundle exec strainer test --log-level=debug'
     end
   end
 end
