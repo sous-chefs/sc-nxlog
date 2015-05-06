42448f20cdd159c35514418a82f238905bc81d1b
Simon Detheridge <simon@widgit.com>
2015-05-06 13:55:06 +0100

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,25 @@
 name             'nxlog'
+version          '0.5.0'
+
+description      'Installs and configures NXLog_ce on Linux or Windows'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+issues_url       'http://github.com/widgit-cookbooks/nxlog/issues'
+source_url       'http://github.com/widgit-cookbooks/nxlog/'
+
+provides         'nxlog::default'
+provides         'nxlog::papertrail'
+provides         'service[nxlog]'
+
+supports         'ubuntu', '>= 12.04'
+supports         'debian', '>= 6.0'
+supports         'redhat', '>= 6.0'
+supports         'centos', '>= 6.0'
+supports         'windows'
+
+recipe           'nxlog::default'
+recipe           'nxlog::papertrail'
+
 maintainer       'Simon Detheridge'
 maintainer_email 'simon@widgit.com'
 license          'Apache v2.0'
-description      'Installs/Configures nxlog'
-long_description 'Installs/Configures nxlog'
-version          '0.1.0'
