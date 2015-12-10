748954804cce4777e8a4f78547f884725df6ff8a
Simon Detheridge <simon@widgit.com>
2015-12-10 11:33:59 +0000

    Bump to 0.8.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.8.3
+
+* Added json extension module
+
 # 0.8.2
 
 * Added checksum validation to nxlog remote_file packages
--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'nxlog'
-version          '0.8.2'
+version          '0.8.3'
 
 description      'Installs and configures NXLog_ce on Linux or Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
