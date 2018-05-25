2535315027ce178e38a3015f7bbeffffa18ec3a8
Graham Davison <gdavison@paybyphone.com>
2018-05-25 12:44:40 -0500

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'nxlog'
+name             'sc-nxlog'
 version          '0.9.0'
 
 description      'Installs and configures NXLog_ce on Linux or Windows'
@@ -6,8 +6,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-issues_url       'http://github.com/widgit-cookbooks/nxlog/issues'
-source_url       'http://github.com/widgit-cookbooks/nxlog/'
+issues_url       'http://github.com/sous-chefs/sc-nxlog/issues'
+source_url       'http://github.com/sous-chefs/sc-nxlog/'
 
 provides         'nxlog::default'
 provides         'nxlog::papertrail'
@@ -24,6 +24,6 @@ recipe           'nxlog::default',    'Install and configure nxlog'
 recipe           'nxlog::papertrail', 'Enable nxlog Papertrail support'
 recipe           'nxlog::syslog',     'Sets up nxlog as the system logger'
 
-maintainer       'Simon Detheridge'
-maintainer_email 'simon@widgit.com'
-license          'Apache v2.0'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
