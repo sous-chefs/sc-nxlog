5abf32c223ef198961770bcb65f019be2f6b8d4d
Graham Davison <gdavison@paybyphone.com>
2018-05-28 13:01:38 -0700

    Removed unneeded fields from `metadata.rb`

--- metadata.rb
+++ metadata.rb
@@ -9,20 +9,11 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url       'http://github.com/sous-chefs/sc-nxlog/issues'
 source_url       'http://github.com/sous-chefs/sc-nxlog/'
 
-provides         'nxlog::default'
-provides         'nxlog::papertrail'
-provides         'nxlog::syslog'
-provides         'service[nxlog]'
-
 supports         'ubuntu', '>= 14.04'
 supports         'debian', '>= 8.0'
 supports         'redhat', '>= 6.0'
 supports         'centos', '>= 6.0'
 supports         'windows'
-
-recipe           'nxlog::default',    'Install and configure nxlog'
-recipe           'nxlog::papertrail', 'Enable nxlog Papertrail support'
-recipe           'nxlog::syslog',     'Sets up nxlog as the system logger'
 
 maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
